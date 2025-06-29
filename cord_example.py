1e39915787046b7d2be6d05853e79e10ff5bf1c0
Simone Sturniolo <simonesturniolo@gmail.com>
2025-06-29 11:35:52 +0100

    Expanded example notebook

--- cord_example.py
+++ cord_example.py
@@ -11,7 +11,7 @@ def _():
     from bayes_cord import cord_test
     from test_data import SyntheticCTable
     import matplotlib.pyplot as plt
-    return (mo,)
+    return cord_test, mo, np, plt
 
 
 @app.cell(hide_code=True)
@@ -61,6 +61,99 @@ def _(mo):
     for a contingency table with elements $a, b, c, d$. Here $I(x;a, b)$ is the [regularized incomplete beta function](https://en.wikipedia.org/wiki/Beta_function#Incomplete_beta_function). A similar integral holds for the upper region, and the middle region can be simply found by subtracting the other two from 1. While the integral is in theory a polynomial, for anything but very small counts it is computationally intractable due to high oscillations and powers, and we use numerical integration instead.
     """
     )
+    return
+
+
+@app.cell(hide_code=True)
+def _(np, plt):
+    def plot_beta_function(ctable: np.ndarray, ax: plt.Axes, n: int = 50):
+        grid = np.linspace(0, 1, n)
+        x, y = np.meshgrid(grid, grid)
+        ctable = np.asarray(ctable)
+        a, b = ctable[:,0]
+        c, d = ctable[:,1]
+
+        z = x**a*(1-x)**b*y**c*(1-y)**d
+
+        ax.set_aspect(1.0)
+        ax.set_xlabel("$p_1$")
+        ax.set_ylabel("$p_2$")
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        ax.pcolormesh(x, y, z)
+
+    def plot_beta_bound(oddr: float, ax: plt.Axes, n: int = 100, c: str = 'w'):
+        x = np.linspace(0, 1, n)
+        y = oddr*x/(1+(oddr-1)*x)
+        ax.plot(x, y, c=c)
+
+    def plot_beta_area(oddr1: float, oddr2: float, ax: plt.Axes, n: int = 100, c: str = 'w', alpha: float =0.3):
+        x = np.linspace(0, 1, n)
+        y1 = oddr1*x/(1+(oddr1-1)*x)
+        if oddr2 == np.inf:
+            y2 = np.ones_like(x)
+        elif oddr2 == -np.inf:
+            y2 = np.zeros_like(x)
+        else:
+            y2 = oddr2*x/(1+(oddr2-1)*x)
+        ax.fill_between(x, y1, y2, color=c, alpha=alpha)
+    return plot_beta_area, plot_beta_bound, plot_beta_function
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    Here is an example of CORD applied to a test contingency table:
+
+    |                  | Did not study | Did study |
+    |------------------|--------------:|----------:|
+    | **Did pass**     |              5|         12|
+    | **Did not pass** |              6|          4|
+
+    The question being asked is whether studying improves or not the chances of passing the test, and how probable it is to do so. $p_1$ will be the probability of passing without studying, and $p_2$ that of passing after studying.
+    """
+    )
+    return
+
+
+@app.cell
+def _(cord_test, np, plot_beta_area, plot_beta_bound, plot_beta_function, plt):
+    _fig, _ax = plt.subplots(1, 3, figsize=(14,4.5))
+
+    # Try multiplying this table by an integer factor to see how a larger sample size with the
+    # same relative frequencies results in higher confidence
+    _test_ctable = np.array([[5, 12], [6, 4]])
+    _log_or_w = 0.6
+
+    _fig.suptitle(f"CORD areas, log odds ratio width: {_log_or_w}")
+
+    _cord_res = cord_test(_test_ctable, _log_or_w)
+    _or_up = _cord_res.or_up
+    _or_down = _cord_res.or_down
+
+    # Colors
+    _c_up = '#fa5555'
+    _c_mid = '#55fa55'
+    _c_down = '#5555fa'
+
+    _ax[0].set_title(f"P: {_cord_res.upper_band:.3f}")
+    plot_beta_function(_test_ctable, _ax[0])
+    plot_beta_bound(_or_up, _ax[0], c=_c_up)
+    plot_beta_area(_or_up, np.inf, _ax[0], c=_c_up)
+
+    _ax[1].set_title(f"P: {_cord_res.middle_band:.3f}")
+    plot_beta_function(_test_ctable, _ax[1])
+    plot_beta_bound(_or_up, _ax[1], c=_c_mid)
+    plot_beta_bound(_or_down, _ax[1], c=_c_mid)
+    plot_beta_area(_or_up, _or_down, _ax[1], c=_c_mid)
+
+    _ax[2].set_title(f"P: {_cord_res.lower_band:.3f}")
+    plot_beta_function(_test_ctable, _ax[2])
+    plot_beta_bound(_or_down, _ax[2], c=_c_down)
+    plot_beta_area(_or_down, -np.inf, _ax[2], c=_c_down)
+
+    _fig
     return
 
 
