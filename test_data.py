0591c98852ee69d70afd581b49aa30535c53057f
Simone Sturniolo <simonesturniolo@gmail.com>
2025-07-02 22:24:15 +0100

    Added option to generate "case control" style data tables

--- test_data.py
+++ test_data.py
@@ -17,26 +17,49 @@ class SyntheticCTable:
         self._odds_ratio = odds_ratio
         self._p2 = p1 * odds_ratio / (1 + p1 * (odds_ratio - 1))
         
-    def generate(self, count: int = 100, p_x: float = 0.5) -> NDArray[np.int32]:
+    def generate(self, count: int = 100, p_x: float = 0.5, case_control: bool = False) -> NDArray[np.int32]:
         """
         Generate a synthetic 2x2 contingency table.
         
         Args:
             count (int): The number of samples to generate for each cell.
             p_x (float): Probability of exposure in the population.
+            case_control (bool): If True, uses a case-control design,
+                meaning the count will be split equally between cases and controls.
         
         Returns:
             np.ndarray: A 2x2 numpy array representing the contingency table.
         """        
         
         # Exposure
-        c1 = self._rng.binomial(count, p_x)
-        c2 = count - c1
+        if case_control:
+            c1 = count // 2
+            c2 = count - c1
+            
+            p_c_not_x = self._p1
+            p_c_x = self._p2
+            p_c = p_c_x * p_x + p_c_not_x * (1 - p_x)
+            p_x_c = p_c_x * p_x / p_c
+            
+            x11 = self._rng.binomial(c1, 1-p_x_c)
+            x12 = c1-x11
+            
+            p_not_c_not_x = 1-self._p1
+            p_not_c_x = 1-self._p2
+            p_not_c = p_not_c_x * p_x + p_not_c_not_x * (1 - p_x)
+            p_x_not_c = p_not_c_x * p_x / p_not_c
+            
+            x21 = self._rng.binomial(c2, 1-p_x_not_c)
+            x22 = c2 - x21
+        else:
+            c1 = self._rng.binomial(count, 1-p_x)
+            c2 = count - c1
         
-        x11 = self._rng.binomial(c1, self._p1)
-        x12 = self._rng.binomial(c2, self._p2)
-        x21 = c1 - x11
-        x22 = c2 - x12
+            x11 = self._rng.binomial(c1, self._p1)
+            x12 = self._rng.binomial(c2, self._p2)
+
+            x21 = c1 - x11
+            x22 = c2 - x12
                 
         return np.array([[x11, x12], [x21, x22]], dtype=np.int32)
     
@@ -45,7 +68,8 @@ class SyntheticCTable:
     
 if __name__ == "__main__":
     # Example usage
-    synthetic_table = SyntheticCTable(p1=0.01, odds_ratio=2.0, seed=42)
+    synthetic_table = SyntheticCTable(p1=0.001, odds_ratio=2.0, seed=42)
     print(synthetic_table)
-    table = synthetic_table.generate(count=1000, p_x=0.5)
+    table = synthetic_table.generate(count=100, p_x=0.3, case_control=True)
     print(table)
+    
